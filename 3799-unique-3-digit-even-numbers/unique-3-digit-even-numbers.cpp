e6b8559e055ee75cc6d9b029403bd784e4b3e1e2
Shivam Vashistha <shivam112205@gmail.com>
2026-09-11 16:46:22 +0530

    Time: 11 ms (42.35%) | Memory: 31.1 MB (33.29%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.cpp
@@ -3,16 +3,14 @@ public:
     int totalNumbers(vector<int>& digits) {
         unordered_set<int> arr;
         for(int i=0;i<digits.size();i++){
-            if(digits[i]==0)
-                continue;
-            for(int j=0;j<digits.size();j++){
-                if(i==j)
-                    continue;
-                for(int k=0;k<digits.size();k++){
-                    if(k==j || k==i)
-                        continue;
-                    if(((digits[i]*100)+(digits[j]*10)+(digits[k]))%2==0)
-                        arr.insert((digits[i]*100)+(digits[j]*10)+(digits[k]));
+            if(digits[i]%2==0){
+                for(int j=0;j<digits.size();j++){
+                    if(i!=j){
+                        for(int k=0;k<digits.size();k++){
+                            if(k!=j && k!=i && digits[k]!=0)
+                                arr.insert((digits[i])+(digits[j]*10)+(digits[k]*100));
+                        }
+                    }
                 }
             }
         }
